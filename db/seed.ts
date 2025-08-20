6e16fdc70972be4cb18095415f956ef84873b408
Nyzuro <marco.strs27@gmail.com>
2025-08-20 10:26:22 +0200

    🗃️ DB > Seed users data

--- db/seed.ts
+++ db/seed.ts
@@ -1,14 +1,18 @@
-import { prisma } from "@/lib/prisma";
-import sampleData from "./sample-data";
+import sampleData from "@/db/sample-data";
+import { PrismaClient } from "@prisma/client";
 
 async function main() {
+	const prisma = new PrismaClient();
 	await prisma.product.deleteMany();
+	await prisma.account.deleteMany();
+	await prisma.session.deleteMany();
+	await prisma.verificationToken.deleteMany();
+	await prisma.user.deleteMany();
 
-	await prisma.product.createMany({
-		data: sampleData.products,
-	});
+	await prisma.product.createMany({ data: sampleData.products });
+	await prisma.user.createMany({ data: sampleData.users });
 
-	console.log("ok");
+	console.log("Database seeded successfully");
 }
 
 main();
